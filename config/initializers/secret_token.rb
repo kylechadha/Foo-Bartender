22a3f7065fbbd906dd3a3e7557b224b1cfa3ebc0
Kyle Chadha <kyle.chadha@gmail.com>
2014-03-07 10:17:24 -0800

    Initial commit, Add gems RSpec Foundation & Guard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FooBartender::Application.config.secret_key_base = '041a803a5751ae8dc0f9475f08943b28c2ca07700f45e214fb056de626edaae8d321f4c4bb3880bf81072bcf8f11ff99682d5f15eea32417949073f451d141da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FooBartender::Application.config.session_store :cookie_store, key: '_foo-bartender_session'
